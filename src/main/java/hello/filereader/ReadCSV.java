f8144c4b58555c9bd5f6b948ad47f036a9cf1888
santosh.natarajan <sa@thevara.com>
2015-04-15 21:39:43 -0600

    creating a hello world to get rest skeleton working

---
+++ src/main/java/hello/filereader/ReadCSV.java
@@ -0,0 +1,15 @@
+package hello.filereader;
+
+import java.io.Reader;
+
+/**
+ * Created by natarajan on 4/15/15.
+ */
+public class ReadCSV {
+
+    public static Reader getReader(){
+        String readString ="";
+        
+
+    }
+}
