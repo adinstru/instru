f4cc8952bf840bb1b310fa071aef2c1fa4bfdce2
santosh.natarajan <sa@thevara.com>
2015-04-15 23:15:17 -0600

    adding integration test cases and load test case work in progress

--- src/main/java/instructure/IntegrationTest.java
+++ src/main/java/instructure/IntegrationTest.java
@@ -6,9 +6,28 @@ import org.testng.annotations.Test;
  * Created by natarajan on 4/15/15.
  */
 public class IntegrationTest {
-    
-    @Test  public void allActiveCourseActiveStudents(){}
-    @Test  public void ZeroActiveCourseAllActiveStudents(){}
-    @Test  public void AllActiveCourseZeroActiveStudents(){}
-    @Test  public void ZeroActiveCourseZeroActiveStudents(){}
+
+    //Happy Test cases
+    @Test  public void allActiveCourseActiveStudentsTest(){}
+    @Test  public void ZeroActiveCourseAllActiveStudentsTest(){}
+    @Test  public void AllActiveCourseZeroActiveStudentsTest(){}
+    @Test  public void ZeroActiveCourseZeroActiveStudentsTest(){}
+    @Test  public void ChangeHeaderOrderCoursesTest(){}
+    @Test  public void ChangeHeaderOrderStudentTest(){}
+
+    //Negative test cases
+    @Test  public void EmptyCourseCsvFileTest(){}
+    @Test  public void InvalidCourseCsvFileTest(){}
+    @Test  public void EmptyStudentCsvFileTest(){}
+    @Test  public void InvalidStudentCsvFileTest(){}
+
+    //Edge case test
+    @Test  public void ReallyLongCourseIDCourseTest(){}
+    @Test  public void NonNumericCourseIDCourseTest(){}
+    @Test  public void ReallyLongCourseNameCourseTest(){}
+    @Test  public void ReallyLongStatusCourseTest(){}
+    @Test  public void InvalidCharacterCourseNameCourseTest(){}
+    @Test  public void CourseNameWithCommaCourseTest(){}
+
+
 }
---
+++ src/main/java/instructure/LoadTest.java
@@ -0,0 +1,12 @@
+package instructure;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by natarajan on 4/15/15.
+ */
+public class LoadTest {
+    @Test public void MaxCourseCsv2000(){}
+    @Test public void MaxStudentCsv2000(){}
+
+}
